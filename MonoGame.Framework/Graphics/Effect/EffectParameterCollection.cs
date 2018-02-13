9f5771c73ef79cb5de514d5938fa0966ee40ca05
LL <l.lober1989@gmail.com>
2018-02-13 15:36:20 +0100

    Removed the lookupByName because it doesn't work with the Water-Effect

--- MonoGame.Framework/Graphics/Effect/EffectParameterCollection.cs
+++ MonoGame.Framework/Graphics/Effect/EffectParameterCollection.cs
@@ -6,23 +6,11 @@ namespace Microsoft.Xna.Framework.Graphics
     {
         internal static readonly EffectParameterCollection Empty = new EffectParameterCollection(new EffectParameter[0]);
 
-        private readonly EffectParameter[] _parameters;
-        private readonly Dictionary<string, int> _indexLookup;
+        private readonly EffectParameter[] _parameters;  
 
         internal EffectParameterCollection(EffectParameter[] parameters)
         {
             _parameters = parameters;
-            _indexLookup = new Dictionary<string, int>(_parameters.Length);
-            for (int i = 0; i < _parameters.Length; i++)
-            {
-                _indexLookup.Add(_parameters[i].Name, i);
-            }
-        }
-
-        private EffectParameterCollection(EffectParameter[] parameters, Dictionary<string, int> indexLookup)
-        {
-            _parameters = parameters;
-            _indexLookup = indexLookup;
         }
 
         internal EffectParameterCollection Clone()
@@ -34,7 +22,7 @@ namespace Microsoft.Xna.Framework.Graphics
             for (var i = 0; i < _parameters.Length; i++)
                 parameters[i] = new EffectParameter(_parameters[i]);
 
-            return new EffectParameterCollection(parameters, _indexLookup);
+            return new EffectParameterCollection(parameters);
         }
 
         public int Count
@@ -42,20 +30,24 @@ namespace Microsoft.Xna.Framework.Graphics
             get { return _parameters.Length; }
         }
 		
-		public EffectParameter this[int index]
-		{
-			get { return _parameters[index]; }
-		}
-		
-		public EffectParameter this[string name]
+        public EffectParameter this[int index]
         {
-            get
+            get { return _parameters[index]; }
+        }
+		
+        public EffectParameter this[string name]
+        {
+            get 
             {
-                int index;
-                if (_indexLookup.TryGetValue(name, out index))
-                    return _parameters[index];
+                // TODO: Add a name to parameter lookup table.
+                foreach (var parameter in _parameters) 
+                {
+                    if (parameter.Name == name) 
+                        return parameter;   
+                }
+
                 return null;
-			}
+            }
         }
 
         public IEnumerator<EffectParameter> GetEnumerator()
